9d040e1a38cdca9c1846f0d1f17c1758a126f4d2
swethakopperla <swethak.itblabs@gmail.com>
2017-12-23 19:29:03 +0530

    u5

---
+++ action.php
@@ -0,0 +1,27 @@
+<html>
+<body>
+<?php
+$name=$_GET['name'];
+$email=$_GET['email'];
+$number=$_GET['number'];
+$date=$_GET['date'];
+$department=$_GET['department'];
+$dname=$_GET['dname'];
+$text=$_GET['comment'];
+$host="localhost";$user="root";
+$pass="Root@1234";$dbname="appointment";
+$conn=mysqli_connect($host,$user,$pass,$dbname);
+if(!$conn)
+die("could not connect to the DB".mysqli_connect_error());
+else
+echo "connected sucessfully............. <br>";
+$sql="insert into patients values('$name','$email','$number','$date','$dapartment','$dname','$text')";
+if(mysqli_query($conn,$sql))
+echo "record sucessfully inserted.......";
+else
+echo "could not insert record:".mysqli_error($conn);
+mysqli_close($conn);
+?>
+</body>
+</html>
+
